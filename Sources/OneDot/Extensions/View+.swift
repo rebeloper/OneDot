6af8d1d2089a46366d92b1efbdfd869134833248
Alex Nagy <rebeloperalpar@gmail.com>
2023-08-22 17:50:43 +0300

    typo fix

--- Sources/OneDot/Extensions/View+.swift
+++ Sources/OneDot/Extensions/View+.swift
@@ -64,7 +64,7 @@ public extension View {
     // Positions this view within an invisible frame with the specified size with a set .center alignment.
     /// - Parameter square: A fixed width and height for the resulting view. If `width` is `nil`, the resulting view assumes this view's sizing behavior.
     /// - Returns: A square view with fixed dimensions of `width` and `height`.
-    func frame(square lenght: CGFloat?) -> some View {
-        self.frame(width: lenght, height: lenght)
+    func frame(square length: CGFloat?) -> some View {
+        self.frame(width: length, height: length)
     }
 }
