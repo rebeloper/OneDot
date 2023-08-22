85fac644a5bbd527b927bbdde40c7e76d2a2d74a
Alex Nagy <rebeloperalpar@gmail.com>
2023-08-22 15:46:22 +0300

    more extensions

---
+++ Sources/OneDot/Extensions/Color+.swift
@@ -0,0 +1,38 @@
+//
+//  Color+.swift
+//
+//
+//  Created by Alex Nagy on 22.08.2023.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    /// Color from HEX
+    /// - Parameter hex: HEX
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(red: Double(r) / 0xff, green: Double(g) / 0xff, blue: Double(b) / 0xff)
+    }
+    
+}
+
+public extension Color {
+    
+    /// Random color
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
--- Sources/OneDot/Extensions/View+.swift
+++ Sources/OneDot/Extensions/View+.swift
@@ -60,4 +60,11 @@ public extension View {
             self
         }
     }
+    
+    // Positions this view within an invisible frame with the specified size with a set .center alignment.
+    /// - Parameter square: A fixed width and height for the resulting view. If `width` is `nil`, the resulting view assumes this view's sizing behavior.
+    /// - Returns: A square view with fixed dimensions of `width` and `height`.
+    func frame(square lenght: CGFloat?) -> some View {
+        self.frame(width: lenght, height: lenght)
+    }
 }
